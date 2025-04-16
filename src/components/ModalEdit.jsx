c231e92bcba96d44a64a4c984f22bb19666636ad
Varun Chauhan <vc28022004@gmail.com>
2025-04-17 01:39:26 +0530

    Update ModalEdit.jsx

--- src/components/ModalEdit.jsx
+++ src/components/ModalEdit.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import "./Modal.css"
-const Modal = ({onClose,addTodos}) => {
+const Modal = ({onClose,addTodos,title,desc}) => {
 
     
   const stop=(e)=>{
     e.stopPropagation()
   }
+  // console.log(title,desc)
 
   return (
     <div className='window' onClick={onClose}>
       <div className='cross' onClick={onClose}>❌</div>
         <div className="Modal"  onClick={stop}>
-            <input type="text"  placeholder='Enter Title' id='todoTitle'/>
-            <textarea name="" id="tododesc" placeholder='write Description'></textarea>
+            <input type="text"  placeholder='Enter Title' id='todoTitle' value={title}/>
+            <textarea name="" id="tododesc" placeholder='write Description' value={desc}></textarea>
             <button onClick={addTodos} className='btn btn2'>Submit</button>
             
         </div>
